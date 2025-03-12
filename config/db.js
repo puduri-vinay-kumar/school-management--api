9a3fc247189e447fe7d3818c175a3874e346319c
puduri-vinay-kumar <vinaykumarpuduri10@gmail.com>
2025-03-12 16:56:19 +0530

    added

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
+
 const db = mysql.createConnection({
-    host: 'mysql-273a70ab-vinaykumarpuduri10-573d.h.aivencloud.com',
-    user: 'avnadmin', // Replace with your MySQL username
-    password: 'AVNS_atY1EVF2W7VjOezhpaO', // Replace with your MySQL password
-    database: 'school_management',
-    port: 23823
+    host: process.env.DB_HOST,
+    user: process.env.DB_USEE,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect((err) => {
